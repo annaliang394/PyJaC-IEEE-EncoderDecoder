dbade7ee84a3f16d436566c69ad5f781509807a6
annaliang394 <34382125+annaliang394@users.noreply.github.com>
2021-01-13 22:48:58 -0500

    Update IEEEConverter.java
    Modified help documentation.
    Removed the IEEEToDec and DecToIEEE classes (to be placed into separate files for readability).

--- IEEEConverter.java
+++ IEEEConverter.java
@@ -1,6 +1,5 @@
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,8 +9,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-import java.util.ArrayList;
-import java.text.DecimalFormat;
 import javafx.geometry.Pos;
 
 /**
@@ -76,7 +73,6 @@ public class IEEEConverter extends Application {
 		mn2helpButton = new Button("Help");
 		mn2helpButton.setPrefSize(100, 40);
 		mn2helpButton.setFont(font);
-	
 
 		// create and format the labels
 		title = new Label("IEEE-754 Floating Point Conversion Tool");
@@ -98,7 +94,6 @@ public class IEEEConverter extends Application {
 
 		Scene mainScene = new Scene(pane, 500, 300);
 
-
 		// -----Help Scene-----
 		VBox pane2 = new VBox(10);
 		pane2.setAlignment(Pos.TOP_CENTER);
@@ -115,41 +110,35 @@ public class IEEEConverter extends Application {
 		// create and format labels for help scene
 		helptitle = new Label("Using the Converter");
 		helptitle.setFont(font_title);
-		use = new Label("                     How to Use:\n"
-				      + "Enter the number (a decimal real number or 32-bit\n"
-				      + "binary value) into the text field. Then, click the\n"
-				      + "corresponding button to convert your value.\n"
-				      + "Note: When converting from binary to decimal, there\n"
-				      + "may be a slight rounding error from the conversion\n"
-				      + "since not every decimal number has a finite representation\n"
-				      + "in binary."
-				      + "\n");
+		use = new Label("                     How to Use:\n" + "Enter the number (a decimal real number or 32-bit\n"
+				+ "binary value) into the text field. Then, click the\n"
+				+ "corresponding button to convert your value.\n"
+				+ "Note: When converting from binary to decimal, there\n"
+				+ "may be a slight rounding error from the conversion\n"
+				+ "since not every decimal number has a finite representation\n" + "in binary." + "\n");
 		use.setFont(font);
 		use.setTextAlignment(TextAlignment.LEFT);
-		spcl = new Label("                 Special Values:\n "
-				      + "Enter 'INFINITY', '-INFINITY', or 'NAN' to get their\n"
-				      + "special IEEE-754 32-bit binary values. This converter\n"
-				      + "also recognizes the difference between 0.0 and -0.0"
-				      + "\n");
+		spcl = new Label(
+				"                 Special Values:\n " + "Enter 'INFINITY', '-INFINITY', or 'NAN' to get their\n"
+						+ "special IEEE-754 32-bit binary values. This converter\n"
+						+ "also recognizes the difference between 0.0 and -0.0" + "\n");
 		spcl.setFont(font);
 		spcl.setTextAlignment(TextAlignment.LEFT);
-		instrct = new Label("                      How it works:\n"
-					  + "When converting from a decimal to an IEEE-754\n"
-					  + "encoded binary, the bits represent (from left to right)\n"
-					  + "   *1 bit for the sign (1 if the decimal is negative,\n "
-					  + "    0 if positive)\n"
-					  + "   *8 bits for the exponent\n"
-					  + "   *23 bits for the mantissa/significand\n"
-					  + "The exponent and mantissa are binary representations of the\n"
-					  + "decimal value in base-2 scientific notation. For more \n"
-					  + "information, visit: https://urlzs.com/DZD97"
-					  + "\n");
+		instrct = new Label("                      How it works:\n" + "When converting from a decimal to an IEEE-754\n"
+				+ "encoded binary, the number can be represented as\n"
+				+ "as: (−1)^{sign bit}*(1+fraction)*2^{exponent - bias}\n"
+				+ "The bits are allocated as follows (left to right):\n"
+				+ "   *1 bit for the sign (1 if the decimal is negative,\n " + "    0 if positive)\n"
+				+ "   *8 bits for the exponent (with bias of 127)\n" + "   *23 bits for the fraction ('mantissa')\n"
+				+ "" + "The exponent and mantissa are binary representations of the\n"
+				+ "decimal value in base-2 scientific notation. For more \n"
+				+ "information, visit: https://urlzs.com/DZD97" + "\n");
 		instrct.setFont(font);
 		instrct.setTextAlignment(TextAlignment.LEFT);
 		contact = new Label("Feel free to contact us at IEEEConverter@gmail.com.");
 		contact.setFont(font);
 		contact.setTextAlignment(TextAlignment.LEFT);
-		
+
 		// add elements to the help scene layout
 		pane2.getChildren().add(helptitle);
 		pane2.getChildren().add(use);
@@ -161,394 +150,6 @@ public class IEEEConverter extends Application {
 		stage.setTitle("IEEE-754 Floating Point Conversion Tool");
 		stage.setScene(mainScene);
 		stage.show();
-	}
-
-}
-
-/**
- * An EventHandler class that knows how to convert user-input from a decimal float to an 
- * IEEE-754 32-bit binary value. 
- */
-class DecToIEEE implements EventHandler<ActionEvent> {
-
-  private TextField tField;
-	private float decimalVal;
-	private Label resultLabel;
-	private boolean isSpecial;
-	private boolean isValid;
-	
-	/**
-	 * Initializes the attributes of this event handler.
-	 * 
-	 * @param tf    A text field containing the decimal float number to convert.
-	 * @param label A label that will display the IEEE-754 representation of the
-	 *              number or an error message.
-	 */
-	public DecToIEEE(TextField tf, Label label) {
-		this.tField = tf;
-		String inputStr = tf.getText();
-		this.resultLabel = label;
-		setDecVal(inputStr);
-	}
-	
-  /**
-  * Checks whether the input String represents a special value or valid decimal number and 
-  * attempts to set the decimal value based on the given String.
-  * If the input is invalid, the decimal value is set to a default of 0.0f
-  * 
-  * @param inputStr a String representing the decimal number the user wishes to convert to binary.
-  */
-	public void setDecVal(String inputStr) {
-		//check if inputStr is a special value
-		if (inputStr.toUpperCase().equals("INFINITY") || inputStr.toUpperCase().equals("-INFINITY")
-				|| inputStr.toUpperCase().equals("NAN")) {
-			this.decimalVal = 0.0f; // placeholder value
-			this.isSpecial = true;
-			this.isValid = true;
-		} else {
-			this.isSpecial = false;
-			//attempt to convert input to a float
-			try {
-				this.decimalVal = Float.parseFloat(inputStr);
-				this.isValid = true;
-			} catch (Exception e) {
-				this.decimalVal = 0.0f; // placeholder value
-				this.isValid = false;
-			}
-		}
-	}
-
-	/**
-	 * Responds to a user clicking the "Convert to Decimal" button.
-	 * 
-	 * @param event An ActionEvent object that indicates that the "Convert to
-	 *              Decimal" button has been clicked.
-	 */
-	public void handle(ActionEvent event) {
-		String inputStr = tField.getText(); // get the updated input string
-		setDecVal(inputStr); // update the decimal value
-		//handle the case where the input from the text field is invalid
-		if (!this.isValid) {
-			this.resultLabel.setText("Invalid input. " + inputStr + " is not a decimal number.");
-			return;
-		} // else: proceed; the input is valid
-		//handle the special value cases
-		if (this.isSpecial) {
-			switch (inputStr.toUpperCase()) {
-			case "INFINITY":
-				this.resultLabel.setText("IEEE-754 Representation: 01111111100000000000000000000000");
-				break;
-			case "-INFINITY":
-				this.resultLabel.setText("IEEE-754 Representation: 11111111100000000000000000000000");
-				break;
-			case "NAN":
-				this.resultLabel.setText("IEEE-754 Representation: 01111111111111111111111111111111");
-				break;
-			}
-			return;
-		//handle the special zero cases
-		} else if (decimalVal == 0.0f) {
-			String sign = getSign(inputStr.charAt(0));
-			this.resultLabel.setText("IEEE-754 Representation: " + sign + "0000000000000000000000000000000");
-			return;
-		} else {
-			String sign = getSign(inputStr.charAt(0));
-			ArrayList<Object> exponentResults = getExponent(this.decimalVal);
-			String exponent = (String) exponentResults.get(0);
-			String mantissa = getMantissa((float) exponentResults.get(1));
-
-			this.resultLabel.setText("IEEE-754 Representation: " + sign + exponent + mantissa);
-			return;
-		}
-	}
-
-	/**
-	 * Returns the IEEE-754 sign-bit based on the given sign character.
-	 * 
-	 * @param signChar a character representing a negative value if and only if it
-	 *                 is equal to '-'
-	 * @return a 1-bit binary string representing the sign
-	 */
-	public String getSign(char signChar) {
-		if (signChar == '-') {
-			return "1";
-		} else {
-			return "0";
-		}
-	}
-
-	/**
-	 * Returns the IEEE-754 exponent of the given decimal floating point.
-	 * 
-	 * @param input a String representing a decimal floating point
-	 * @return an ArrayList containing an 8-bit binary string representing the
-	 *         exponent and a float representing the decimal value of the Mantissa
-	 */
-	public ArrayList<Object> getExponent(float input) {
-		int rawExp = 0;
-		float mantissaNum = input;
-		//calculate the integer that gives the nearest power of 2 to <input>
-		if (mantissaNum < 1) {
-			while (mantissaNum < 1) {
-				mantissaNum = mantissaNum * 2;
-				rawExp--;
-			}
-		} else { // mantissaNum >= 1
-			while (mantissaNum >= 2) {
-				mantissaNum = mantissaNum / 2;
-				rawExp++;
-			}
-		}
-		//the IEEE standard is to bias the exponent value by 127
-		//find the corresponding binary string for the biased exponent
-		String biasedExpStr = Integer.toBinaryString(rawExp + 127);
-		while (biasedExpStr.length() < 8) {
-			biasedExpStr = "0" + biasedExpStr;
-		}
-		ArrayList<Object> results = new ArrayList<>(); 
-		results.add(biasedExpStr);
-		results.add(mantissaNum);
-		return results;
-	}
-
-	/**
-	 * Returns the IEEE-754 Mantissa of the given decimal floating point.
-	 * 
-	 * @param input a decimal floating point number
-	 * @return a 23-bit binary string representing the Mantissa
-	 */
-	public String getMantissa(float input) {
-		DecimalFormat df = new DecimalFormat("0.00");
-		float val = input - 1;
-		String retStrRaw = "";
-		//calculate 26 bits for the un-rounded Mantissa
-		for (int i = 0; i < 26; i++) {
-			val = Float.parseFloat(df.format(val * 2));
-			if (val >= 1) {
-				retStrRaw = retStrRaw + "1";
-				val = val - 1;
-			} else {
-				retStrRaw = retStrRaw + "0";
-			}
-		}
-		String retStr = roundMantissa(retStrRaw);
-		return retStr;
-	}
-
-	/**
-	 * Returns the binary string rounded to the nearest even.
-	 * PRECONDITION: the given String <man> is a 26-bit binary String
-	 * 
-	 * @param man a 26-bit binary String (representing the un-rounded Mantissa)
-	 * @return the binary String <man> rounded to a 23-bit binary String
-	 */
-	public String roundMantissa(String man) {
-		String lastThreeDigits = man.substring(23);
-		String first23Digits = man.substring(0, 23);
-		String roundedMan = "";
-		if (lastThreeDigits.startsWith("0")) {
-			// round down
-			roundedMan = first23Digits;
-		} else if (lastThreeDigits.equals("110") || lastThreeDigits.equals("101") || lastThreeDigits.equals("111")) {
-			// round up
-			roundedMan = roundUp(first23Digits);
-		} else {
-			// lastThreeDigits.equals("100")
-			if (man.charAt(22) == '1') {
-				// round up
-				roundedMan = roundUp(first23Digits);
-			} else {
-				// round down
-				roundedMan = first23Digits;
-			}
-		}
-		return roundedMan;
-	}
-
-	/**
-	 * Rounds up the given binary string if and only if rounding up does not add any
-	 * more bits. If rounding up results in adding another bit, the original string
-	 * is returned.
-	 * 
-	 * @param binStr a String representing the binary value to be rounded up
-	 * @return a String representing the rounded binary string or the original
-	 *         String if rounding cannot be done without adding more bits
-	 */
-	public String roundUp(String binStr) {
-		int currIndex = binStr.length() - 1;
-		int numZeroes = 0;
-		String zeroes = "";
-		String retStr = binStr;
-		while (binStr.charAt(currIndex) == '1' && currIndex > 1) {
-			currIndex--;
-			numZeroes++;
-		}
-		for (int i = 0; i < numZeroes; i++) {
-			zeroes = zeroes + "0";
-		}
-		if (currIndex == 1 && binStr.charAt(1) == '1' && binStr.charAt(0) == '1') {
-			// do nothing; the number cannot be rounded up without adding more bits
-		} else {
-			retStr = retStr.substring(0, currIndex) + "1" + zeroes;
-		}
-		return retStr;
-	}
-
-}
-
-/**
- * An EventHandler class that converts user-input from an 
- * IEEE-754 32-bit binary value to a decimal float.
- */
-class IEEEToDec implements EventHandler<ActionEvent> {
-	private String inputstr;
-	private Label label;
-	private int sign;
-	private int exponent;
-	private double mantissa;
-	private TextField tf;
-	
-	/**
-	 * Sets the sign, exponent and mantissa of the IEEE 754 floating point number.
-	 * 
-	 * @param tf    A text field containing the IEEE 754 floating point number.
-	 * @param label A label that will display the decimal representation of the number. 
-	 */
-	public IEEEToDec(TextField tf, Label label) {
-		//Initialize the attributes belonging to the IEEEToDec class
-		this.label = label;
-		this.tf = tf;
-		this.inputstr = tf.getText();
-
-	}
-	
-	/**
-	 * Sets the label to the error message if the user input is invalid.
-	 * 
-	 */
-	public void error() {
-		//Set the label to display the error message
-		this.label.setText("Invalid entry. Please enter a 32-bit binary value.");
-	}
-	
-	/**
-	 * Determines whether the IEEE 754 input is a special value. If it is, updates
-	 * the label to what special value the input is and returns true. Otherwise, 
-	 * returns false.
-	 * 
-	 * @param tf    A text field containing the IEEE 754 floating point number to convert.
-	 * @param label A label that will display the decimal representation of the
-	 * @return true if the input is a special value, false otherwise.            
-	 */
-	public boolean check_spcl(String input) {
-		//The decimal representation is equal to infinity
-		if (input.equals("01111111100000000000000000000000")) {
-			this.label.setText("Decimal Representation: INFINITY");
-			return true;
-		//The decimal representation is equal to -infinity
-		} else if (input.equals("11111111100000000000000000000000")) {
-			this.label.setText("Decimal Representation: -INFINITY");
-			return true;
-		//The decimal representation is equal to NAN
-		} else if (input.equals("01111111111111111111111111111111")) {
-			this.label.setText("Decimal Representation: NAN");
-			return true;
-
-		}
-		return false;
-
-	}
-	
-	/**
-	 * Sets the sign to be 0 if the IEEE 754 floating point number begins with a 0
-	 * or 1 if it begins with a 1. Otherwise, displays an error message.
-	 * 
-	 * @param input The first character of the IEEE 754 floating point number.
-	 */
-	public void getsign(char input) {
-		//Assign sign as 0 if the first character is 0
-		if (input == '0') {
-			this.sign = 0;
-		//Assign sign as 1 if the first character is 1
-		} else if (input == '1') {
-			this.sign = 1;
-		} else {
-			error();
-			return;
-		}
-	}
-	
-	/**
-	 * If possible, determines the exponent of the IEEE 754 floating point number.
-	 * Otherwise, displays an error message.
-	 * 
-	 * @param input A string that is the binary representation of the exponent of
-	 *              the IEEE 754 floating point number.
-	 */
-	public void getexponent(String input) {
-		try {
-			//Convert from binary to decimal
-			int decimal = Integer.parseInt(input, 2);
-			//Subtract the bias to determine the exponent
-			this.exponent = decimal - 127;
-
-		}
-
-		catch (Exception e) {
-			error();
-			return;
-		}
-
-	}
-	
-	/**
-	 * If possible, determines the mantissa of the IEEE 754 floating point number.
-	 * Otherwise, displays an error message.
-	 * 
-	 * @param input A string that is the binary representation of the mantissa of
-	 *              the IEEE 754 floating point number.
-	 */
-	public void getmantissa(String input) {
-		double decimal = 0;
-		int num = 0;
-		//Converts the mantissa to a decimal value
-		for (int i = -1; i > -24; i--) {
-			char chr = input.charAt(num);
-			int value = Integer.parseInt(String.valueOf(chr));
-			if (value > 1) {
-				error();
-				break;
-			}
-			decimal += (value * Math.pow(2, i));
-			num++;
-		}
-		this.mantissa = decimal;
-	}
-	
-	/**
-	 * Responds to a user clicking the Convert to IEEE button.
-	 * 
-	 * @param event An ActionEvent object that indicates that the Convert to IEEE
-	 *              button has been clicked.
-	 */
-	public void handle(ActionEvent event) {
-		this.inputstr = this.tf.getText();
-		if (!(this.inputstr.length() == 32)) {
-			error();
-			return;
-		}
-		//If the user input is a special value
-		if (check_spcl(this.inputstr)) {
-			return;
-		}
-		//If the user input is not a special value
-		getsign(this.inputstr.charAt(0));
-		getexponent(this.inputstr.substring(1, 9));
-		getmantissa(this.inputstr.substring(9));
-		double number = Math.pow(-1, this.sign) * (1 + this.mantissa) * Math.pow(2, this.exponent);
-		this.label.setText("Value Actually Stored in Float: " + String.valueOf(number));
-		return;
-
 	}
 
 }
